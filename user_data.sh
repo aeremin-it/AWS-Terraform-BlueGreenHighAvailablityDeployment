2750e77b4da200771b514b91df8e1b9d2843703c
Your Name <you@example.com>
2021-09-07 19:30:00 -0500

    Initial commit

---
+++ user_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+cat <<EOF > /var/www/html/index.html
+<html>
+<body bgcolor="black">
+<h2><font color="gold">Build by Power of Terraform</h2><br><p>
+<font color="aqua">Server Private IP: $myip<br><br>
+<font color="white">
+<b>Version 1.0</b>
+</body>
+</html>
+EOF
+service httpd start
+systemctl enable httpd
